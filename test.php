1c3ec418d56017f1a777353c04fbb5376b9b378b
ZiK93 <93lrojas@gmail.com>
2022-08-10 11:56:25 -0600

    teting create table

--- test.php
+++ test.php
@@ -10,13 +10,24 @@ $dbname = "id16779907_pedidos_ray";
        die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql33 = 'SELECT id_pedido FROM pedido';
+    /* $sql33 = 'SELECT id_pedido FROM pedido';
     if ($conn->query($sql33)) {
         echo $conn->query($sql33)->num_rows;
     } else {
         echo $conn->query($sql33)->num_rows;
     }
-
+    */
+    $sql33 = 'CREATE TABLE `cliente` (
+        `id_cliente` int(11) NOT NULL,
+        `nombre` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+        `completado` int(11) NOT NULL DEFAULT 0
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci';
+    if ($conn->query($sql33)) {
+        echo "success"
+    } else {
+        echo "error";
+    }
+    ;
     $conn->close();
 
 ?>

992e37bfcc2a9eb5d5942b6ce12eef8616b56f2f
ZiK93 <93lrojas@gmail.com>
2022-08-10 12:02:47 -0600

    Update test.php

--- test.php
+++ test.php
@@ -17,17 +17,15 @@ $dbname = "id16779907_pedidos_ray";
         echo $conn->query($sql33)->num_rows;
     }
     */
-    $sql33 = 'CREATE TABLE `cliente` (
-        `id_cliente` int(11) NOT NULL,
-        `nombre` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-        `completado` int(11) NOT NULL DEFAULT 0
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci';
+    $sql33 = 'ALTER TABLE `cliente`
+    ADD PRIMARY KEY (`id_cliente`)';
     if ($conn->query($sql33)) {
         echo "success";
     } else {
         echo "error";
     }
-    ;
+
+    
     $conn->close();
 
 ?>

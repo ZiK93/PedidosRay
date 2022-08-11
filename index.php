04a5b4e83bd8459251754dbcdcec0054c24d7064
ZiK93 <93lrojas@gmail.com>
2022-08-10 22:17:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.min.js"></script>
 
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
 
 
         <link rel="stylesheet" href="css/style.css">
-        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>    
     </head>
     <body>
     <?php $verificado = verificarPedidos(); ?>
@@ -42,6 +42,7 @@
                     <select id="clients" name="clients_list" form="clients_list" class="form-control selectpicker" data-live-search="true">
                         <option disabled selected value> -- </option>
                         <?php
+                        
                         $clientes = obtenerClientes();
                         foreach($clientes as $id => $cliente){
                             if($cliente["completado"] == 0){

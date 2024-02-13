a4fc74423d8008d8272bc0477f0319842ce62d43
ZiK93 <93lrojas@gmail.com>
2024-02-12 19:22:26 -0600

    error mysql

--- nuevoproducto.php
+++ nuevoproducto.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$insertProducto = "INSERT INTO `producto` (`slug`, `nombre`) VALUES ('choppers_esp', 'CHOPPERS'),('crujipan_esp', 'Crujipan')('pmundo_cartago_esp', 'Pequeño mundo cartago')";
+$insertProducto = "INSERT INTO `producto` (`slug`, `nombre`) VALUES ('choppers_esp', 'CHOPPERS'),('crujipan_esp', 'Crujipan'),('pmundo_cartago_esp', 'Pequeño mundo cartago')";
 if ($conn->query($insertProducto)) {
     echo "<br> success insert<br>  Producto 12 febrero diciembre - choppers_esp crujipan_esp pmundo_cartago_esp<br>";
 } else {

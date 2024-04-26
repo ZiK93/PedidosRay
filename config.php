69bb16ef2cfabf32e4a5f6923e3f0192ce1096f5
ZiK93 <93lrojas@gmail.com>
2024-04-26 12:09:28 -0600

    update pswd

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ function connect_to_database()
 	
 	$servername = "testmysqlpedidosray.mysql.database.azure.com";
     $username = "pedidosray";
-    $password = ".,05zaxscdvf11";
+    $password = ".,05zaxscdvf112233";
     $dbname = "id16779907_db2";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
